b093bccfbe9fdf3ad1fd96696cd07712c3857328
SyedUzair42101 <uzairhassan206@gmail.com>
2022-07-27 17:33:48 +0500

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             /* 
+              Data types
+             */
+
+            int a = 4; //int
+            float b = 0; //float
+            char c = 'a';   //char
+            bool d = false; //float
+            String e = "uzair"; //String
+
+            //Print 
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine("MY First C# code");
+            //Read to commandline 
+            Console.ReadLine();
+        }
+    }
+}
